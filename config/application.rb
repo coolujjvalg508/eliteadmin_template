44d448e9eb524cd1fcf34578003901ce79929a6f
nitin <nitin.singh@brsoftech.org>
2016-09-02 17:56:45 +0530

    update

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module Cgmeetuplatest
     config.assets.paths << Rails.root.join("app", "assets", "fonts","images")
 	config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 	
+	config.action_dispatch.default_headers = {
+		'X-Frame-Options' => 'ALLOWALL'
+	}
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
