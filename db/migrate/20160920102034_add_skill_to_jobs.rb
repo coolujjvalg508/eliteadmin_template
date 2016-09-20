5045df6c94ade8666720ee72a959f2ee59f545a9
nitin <nitin.singh@brsoftech.org>
2016-09-20 18:16:09 +0530

    Update jobs

--- db/migrate/20160920102034_add_skill_to_jobs.rb
+++ db/migrate/20160920102034_add_skill_to_jobs.rb
@@ -1,7 +1,12 @@
 class AddSkillToJobs < ActiveRecord::Migration
   def change
-   add_column :jobs, :skill, :json
-   add_column :jobs, :software_expertise, :json
-   add_column :jobs, :where_to_show, :json
+   remove_column :jobs, :skill
+   remove_column :jobs, :software_expertise
+   remove_column :jobs, :where_to_show
+   
+   
+  # add_column :jobs, :skill, :json
+  # add_column :jobs, :software_expertise, :json
+  # add_column :jobs, :where_to_show, :json
   end
 end
