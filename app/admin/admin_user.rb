a089e09a8f6e458c9311a4ff64ff8fa7fc51a68e
nitin <nitin.singh@brsoftech.org>
2016-10-12 17:54:27 +0530

    update

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -56,5 +56,47 @@ ActiveAdmin.register AdminUser do
     
   end
   
+   show do
+		attributes_table do
+		  row :email
+		  row :Encrypted_Password do |rp|
+		    rp.encrypted_password
+		  end
+		  row :Reset_Password_Token do |rp|
+		    rp.reset_password_token
+		  end
+		  row :Reset_Password_Sent_At do |rp|
+		    rp.reset_password_sent_at
+		  end
+		  
+		  row :Remember_Created_At do |rp|
+		    rp.remember_created_at
+		  end
+		  
+		  row :sign_in_count do |rp|
+		    rp.sign_in_count
+		  end
+		 
+		  row :current_sign_in_at do |rp|
+		    rp.current_sign_in_at
+		  end
+		   row :last_sign_in_at do |rp|
+		    rp.last_sign_in_at
+		  end
+		   row :current_sign_in_ip do |rp|
+		    rp.current_sign_in_ip
+		  end
+		   row :last_sign_in_ip do |rp|
+		    rp.last_sign_in_ip
+		  end
+		 
+		  row :group_id do |rp|
+				UserGroup.find_by(id: rp.group_id).try(:name)
+		  end
+
+		  row :created_at
+		end
+    end
+  
 
 end
