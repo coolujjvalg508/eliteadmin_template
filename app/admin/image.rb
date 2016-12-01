f7237fcf80e7a67686d77c5821066b1475c67198
nitin <nitin.singh@brsoftech.org>
2016-12-01 13:56:17 +0530

    update

--- app/admin/image.rb
+++ app/admin/image.rb
@@ -26,7 +26,7 @@ ActiveAdmin.register Image do
 				uploaded_io = params[:file]
 				new_name	=	Time.now.to_i
 				filename_new	=	''
-				File.open(Rails.root.join('public', 'uploads/tinymic', uploaded_io.original_filename), 'wb') do |file|
+				File.open(Rails.root.join('/public', 'uploads/tinymic', uploaded_io.original_filename), 'wb') do |file|
 						File.rename(file, 'public/uploads/tinymic/'+new_name.to_s + File.extname(file))
 						filename_new	=	new_name.to_s + File.extname(file)
 						file.write(uploaded_io.read)
