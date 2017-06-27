3d40fce0217981457864c721eaf4d028c918cc8a
nitin <nitin.singh@brsoftech.org>
2017-06-27 17:46:05 +0530

    update

--- db/migrate/20170620045954_addisurgent_fields_totables.rb
+++ db/migrate/20170620045954_addisurgent_fields_totables.rb
@@ -1,7 +1,7 @@
 class AddisurgentFieldsTotables < ActiveRecord::Migration
   def change
 
-  	#add_column :jobs, :is_urgent, :boolean, :default => false
+  	#add_column :jobs, :is_urgent, :boolean, :default => false 
   	add_column :news, :is_urgent, :boolean, :default => false
   end
 end
